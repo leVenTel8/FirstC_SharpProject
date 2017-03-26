e417f2aa3f51c9d4a55bd3a6d035872967fed51d
Vitaly Frolov <leventel8@yandex.ru>
2017-03-26 22:51:27 +0400

    первый C# проект

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondApp
+{
+    class Program
+    {
+        // передача параметра с params
+        static void Addition(params int[] integers)
+        {
+            int result = 0;
+            for (int i = 0; i < integers.Length; i++)
+            {
+                result += integers[i];
+            }
+            Console.WriteLine(result);
+        }
+        // передача массива
+        static void AdditionMas(int[] integers, int k)
+        {
+            int result = 0;
+            for (int i = 0; i < integers.Length; i++)
+            {
+                result += (integers[i] * k);
+            }
+            Console.WriteLine(result);
+        }
+
+        static void Main(string[] args)
+        {
+            Addition(1, 2, 3, 4, 5);
+
+            int[] array = new int[] { 1, 2, 3, 4 };
+            AdditionMas(array, 2);
+
+            Console.ReadLine();
+        }
+    }
+}
